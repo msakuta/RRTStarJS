07d76da00f1045065a5846ae6c4c5dc8a4ee64a1
msakuta <masahiro.sakuta@gmail.com>
2021-05-09 12:09:15 +0900

    Check hit with Room walls

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-
-
 const MAX_SPEED = 2.;
 
 class Car{
@@ -58,11 +56,62 @@ class Room {
         ];
     }
 
-    render(ctx: CanvasRenderingContext2D){
+    render(ctx: CanvasRenderingContext2D, highlight: number | null){
+        if(highlight !== null){
+            ctx.strokeStyle = "#f00";
+            ctx.lineWidth = 5;
+            ctx.beginPath();
+            const v0 = this.walls[highlight];
+            const v1 = this.walls[(highlight + 1) % this.walls.length];
+            ctx.moveTo(v0[0], v0[1])
+            ctx.lineTo(v1[0], v1[1]);
+            ctx.stroke();
+        }
+        ctx.strokeStyle = "#000";
+        ctx.lineWidth = 1;
         ctx.beginPath();
         this.walls.forEach((pos) => ctx.lineTo(pos[0], pos[1]));
         ctx.closePath();
         ctx.stroke();
+    }
+
+    checkHit(car: Car): number[] | null {
+        function zipAdjacent(a1: any[]) {
+            let ret = [];
+            for(let i = 0; i < a1.length-1; i++){
+                ret.push([a1[i], a1[i+1]]);
+            }
+            return ret;
+        }
+        function zipAdjacentReduce(a: any[], fn: (a: any, b: any, acc: any) => any, initialValue: any) {
+            let ret = initialValue;
+            for(let i = 0; i < a.length-1; i++){
+                ret = fn(a[i], a[i+1], ret);
+            }
+            return ret;
+        }
+        let hit = zipAdjacent(this.walls).reduce((acc: number[] | null, [pos, nextPos]: number[][], idx: number) => {
+            function distanceToLine(line0: number[], line1: number[], pos: number[]): number {
+                const direction = [line1[0] - line0[0], line1[1] - line0[1]];
+                const length = Math.sqrt(direction[0] * direction[0] + direction[1] * direction[1]);
+                direction[0] /= length;
+                direction[1] /= length;
+                const normal = [direction[1], -direction[0]];
+                let directionComp = direction[0] * (pos[0] - line0[0]) + direction[1] * (pos[1] - line0[1]);
+                if(directionComp < 0){
+                    const delta = [pos[0] - line0[0], pos[1] - line0[1]];
+                    return Math.sqrt(delta[0] * delta[0] + delta[1] * delta[1]);
+                }
+                else if(length < directionComp){
+                    const delta = [pos[0] - line1[0], pos[1] - line1[1]];
+                    return Math.sqrt(delta[0] * delta[0] + delta[1] * delta[1]);
+                }
+                return Math.abs(normal[0] * (pos[0] - line0[0]) + normal[1] * (pos[1] - line0[1]));
+            }
+            const dist = distanceToLine(pos, nextPos, [car.x, car.y]);
+            return acc ?? (dist < 10 ? [idx, dist] : null);
+        }, null);
+        return hit;
     }
 }
 
@@ -72,15 +121,16 @@ let room = new Room();
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 function render(){
     const ctx = canvas?.getContext("2d");
+    const hit = room.checkHit(car);
     if(ctx){
         const {width, height} = canvas.getBoundingClientRect();
         ctx.clearRect(0, 0, width, height);
         car.render(ctx);
-        room.render(ctx);
+        room.render(ctx, hit ? hit[0] : null);
     }
     const carElem = document.getElementById("car");
     if(carElem)
-        carElem.innerHTML = `${car.x}, ${car.y}`;
+        carElem.innerHTML = `${car.x.toFixed(2)}, ${car.y.toFixed(2)}, hit: ${hit}`;
 }
 
 class ButtonState{
